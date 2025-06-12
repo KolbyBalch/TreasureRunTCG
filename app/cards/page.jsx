e024711ef3bbfe9139b23b635c84c4b2e1bb959b
KolbyBalch <kolby.balch@gmail.com>
2025-06-11 22:44:55 -0500

    added details page(s)

--- app/cards/page.jsx
+++ app/cards/page.jsx
@@ -3,9 +3,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from "react";
+import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
+    const searchParams = useSearchParams()
+    const id = Number(searchParams.get('id'))
+
     const [cards, setCards] = useState(null)
+    const [selectedCard, setSelectedCard] = useState(null)
     const [isLoading, setLoading] = useState(true)
     
     useEffect(() => {
@@ -19,13 +24,35 @@ export default function Page() {
     }, [])
 
     useEffect(() => {
-        console.log(cards?.data[0]);
-    }, [cards]);
+        if (cards?.data.length > 0) {
+            setSelectedCard(cards?.data.filter(card => card.id === id)[0])
+        }
+    }, [id, cards])
+
 
     if (isLoading) return <p>Loading...</p>
-    if (!cards) return <p>No profile data</p>
+    if (!cards) return <p>Sorry! We can't find the cards you are looking for... Check back later.</p>
 
-
+    if (!!id) return (
+        <div className="flex flex-col gap-12 sm:gap-16">
+            <Link href='/cards'>Back</Link>
+            <div className="card-details flex flex-wrap gap-x-4 gap-y-1">
+                <Image 
+                    width="825" 
+                    height="1125" 
+                    src={`https://raw.githubusercontent.com/KolbyBalch/TreasureRun/refs/heads/main/assets/cardFronts/alp_${String(selectedCard?.id).padStart(3, '0')}.png`}
+                    style={{borderRadius: "12px"}}
+                    alt={`Image of ${selectedCard?.name}.`}
+                />
+                <div className="info">
+                    <h1>{selectedCard?.name}</h1>
+                    <h2>{selectedCard?.rarity}</h2>
+                    <h2>{selectedCard?.type}{selectedCard?.subtype ? ` - ${selectedCard?.subtype}` : ''}</h2>
+                    <p>{selectedCard?.rulesText}</p>
+                </div>
+            </div>
+        </div>
+    );
 
     return (
         <div className="flex flex-col gap-12 sm:gap-16">
@@ -33,7 +60,7 @@ export default function Page() {
             <div className="card-list flex flex-wrap gap-x-4 gap-y-1">
                 {cards.data.map((card, index) => (
                     <div key={index} className="card">
-                        <Link href={'/cards'} className="inline-flex px-1.5 py-1 sm:px-3 sm:py-2">
+                        <Link href={`/cards?id=${card.id}`} className="inline-flex px-1.5 py-1 sm:px-3 sm:py-2">
                             <Image 
                                 width="825" 
                                 height="1125" 
@@ -47,4 +74,7 @@ export default function Page() {
             </div>
         </div>
     );
+
 }
+
+export const dynamicParams = true;
